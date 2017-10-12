6e26b762a3d20fa491a739e3578c5fbe6f536dbb
shitou <2832904090@qq.com>
2017-10-12 22:17:47 +0800

    初始化app

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,44 @@
 <template>
   <div id="app">
-
+    <mt-tab-container class="page-tabbar-container" v-model="selected">
+      <mt-tab-container-item id="外卖">
+        餐厅
+      </mt-tab-container-item>
+      <mt-tab-container-item id="订单">
+        订单
+      </mt-tab-container-item>
+      <mt-tab-container-item id="发现">
+        发现
+      </mt-tab-container-item>
+      <mt-tab-container-item id="我的">
+        我的
+      </mt-tab-container-item>
+    </mt-tab-container>
+    <mt-tabbar v-model="selected" fixed>
+      <mt-tab-item id="外卖">
+        外卖
+      </mt-tab-item>
+      <mt-tab-item id="订单">
+        订单
+      </mt-tab-item>
+      <mt-tab-item id="发现">
+        发现
+      </mt-tab-item>
+      <mt-tab-item id="我的">
+        我的
+      </mt-tab-item>
+    </mt-tabbar>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
 
-export default {
-  name: 'app',
-  components: {
-    Hello
+  export default {
+    name: 'app',
+    data() {
+      return {
+        selected: '外卖'
+      }
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 
-Vue.config.productionTip = false
+Vue.use(MintUI);
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
-})
+});
